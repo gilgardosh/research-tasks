689a00c73f949b049f80fb4d24e8078916a92160
Gil Gardosh <gilgardosh@gmail.com>
2020-10-28 11:55:45 +0200

    stages remarks

--- src/app/values-ranking/values-ranking.component.ts
+++ src/app/values-ranking/values-ranking.component.ts
@@ -7,11 +7,14 @@ import { credentials } from '../models';
   styleUrls: ['./values-ranking.component.scss'],
 })
 export class ValuesRankingComponent implements OnInit {
-  /*  Stages:
-      1 - entering form
-      2 - opening
-      3 - introduce-values
-  */
+  /**
+   * Stages:
+   * 1 - entering form
+   * 2 - introduce values set 1
+   * 3 - rank values set 1
+   * 4 - introduce values set 2
+   * 5 - rank values set 2
+   */
   stage: number = 1;
   creds: credentials;
 
