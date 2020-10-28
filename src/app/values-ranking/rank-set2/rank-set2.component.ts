58a438c7f1440aabcd7f33211924a9d9bf00928b
Gil Gardosh <gilgardosh@gmail.com>
2020-10-28 11:44:46 +0200

    rank-set2 comp. created

---
+++ src/app/values-ranking/rank-set2/rank-set2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rank-set2',
+  templateUrl: './rank-set2.component.html',
+  styleUrls: ['./rank-set2.component.scss']
+})
+export class RankSet2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
