ec9d0c818124765d88b211c27f9171f925027670
Gil Gardosh <gilgardosh@gmail.com>
2020-10-30 12:35:30 +0200

    remove redundant class

--- src/app/models/index.ts
+++ src/app/models/index.ts
@@ -1,2 +1,1 @@
 export * from './credentials.interface';
-export * from './values-data.interface';
--- src/app/values-ranking/summary/summary.component.ts
+++ src/app/values-ranking/summary/summary.component.ts
@@ -8,10 +8,18 @@ import { AudioService } from 'src/app/shared/services/audio.service';
 })
 export class SummaryComponent implements OnInit {
   @Input() data: any;
+  veryImg1: string = '';
+  veryImg2: string = '';
+  notImg1: string = '';
+  notImg2: string = '';
 
   constructor(private audioService: AudioService) {}
 
   ngOnInit(): void {
+    this.veryImg1 = `../../assets/values-ranking/values_img/val${this.data['rank-set1'].veryvery.valNum}.png`;
+    this.veryImg1 = `../../assets/values-ranking/values_img/val${this.data['rank-set2'].veryvery.valNum}.png`;
+    this.notImg1 = `../../assets/values-ranking/values_img/val${this.data['rank-set1'].notnot.valNum}.png`;
+    this.notImg1 = `../../assets/values-ranking/values_img/val${this.data['rank-set2'].notnot.valNum}.png`;
     this.audioService.setAudio(
       `../../assets/values-ranking/guidance_aud/end-${
         this.data.creds.gender === 'male' ? 'M' : 'F'
