96f4ba623433b4576c6b2d2da94811d5b8a5993b
Gil Gardosh <gilgardosh@gmail.com>
2020-10-30 16:16:49 +0200

    final data object func.

--- src/app/values-ranking/values-ranking.component.ts
+++ src/app/values-ranking/values-ranking.component.ts
@@ -53,11 +53,36 @@ export class ValuesRankingComponent implements OnInit {
 
   scene5(endFlag: boolean) {
     if (endFlag) {
+      this.calculateData();
       this.scene = 6;
     }
   }
   calculateData() {
-    let finalData = {};
+    let finalData = {
+      schoolID: this.dataService.schoolID,
+      childID: this.dataService.childID,
+      gender: this.dataService.gender,
+      pbvs1: this.dataService.pbvs1.rank,
+      pbvs2: this.dataService.pbvs2.rank,
+      pbvs3: this.dataService.pbvs3.rank,
+      pbvs4: this.dataService.pbvs4.rank,
+      pbvs5: this.dataService.pbvs5.rank,
+      pbvs6: this.dataService.pbvs6.rank,
+      pbvs7: this.dataService.pbvs7.rank,
+      pbvs8: this.dataService.pbvs8.rank,
+      pbvs9: this.dataService.pbvs9.rank,
+      pbvs10: this.dataService.pbvs10.rank,
+      pbvs11: this.dataService.pbvs11.rank,
+      pbvs12: this.dataService.pbvs12.rank,
+      pbvs13: this.dataService.pbvs13.rank,
+      pbvs14: this.dataService.pbvs14.rank,
+      pbvs15: this.dataService.pbvs15.rank,
+      pbvs16: this.dataService.pbvs16.rank,
+      pbvs17: this.dataService.pbvs17.rank,
+      pbvs18: this.dataService.pbvs18.rank,
+      pbvs19: this.dataService.pbvs19.rank,
+      pbvs20: this.dataService.pbvs20.rank,
+    };
     console.log(finalData);
   }
 }
