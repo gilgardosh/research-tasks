eea1324a140e550dbdbd734bee66644de67dcf4e
Gil Gardosh <gilgardosh@gmail.com>
2020-11-06 14:11:53 +0200

    stickers basic design

--- src/app/stickers/stickers.component.ts
+++ src/app/stickers/stickers.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-stickers',
   templateUrl: './stickers.component.html',
-  styleUrls: ['./stickers.component.scss']
+  styleUrls: ['./stickers.component.scss'],
 })
 export class StickersComponent implements OnInit {
+  title: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
